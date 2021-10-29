09de44c6bc42c08a46c5bb031b308557e321d017
lulup52 <lucas-p52@hotmail.fr>
2021-10-29 12:12:58 +0200

    la porté de chaque props s'affiche bien, reste a la retirer

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -45,70 +45,117 @@ export default function GameSection({selectedProps, hintActivated}) {
   }
   /*---------------------gestion du hover en fonction de si la tuile est vide ou non-------------------------*/
 
-const checkEmpty = (e) => {
+const hoverCheck = (e) => {
   let targetBehavior = e.target.dataset.behavior
   if(targetBehavior  !== "basicTile") {
     /*verification de l'activation du bouton hint*/
     if (!hintActivated) {
+      
       /*si le bouton hint n'est pas activé, la verif de la case se fait normalement*/
       e.target.classList.add("redTile")
 
     } else {
-      /*si le bouton hint activé,la verif de la case ne se fait mais on affichera la porté de tous 
-      les props du même type que la case survolée*/
 
-      console.log(`ceci est un ${targetBehavior }`)
-      let propsListe = document.getElementsByClassName(`boardTile ${targetBehavior}`)
-      let allCaseToCheck = []
-      for(let i = 0; i < propsListe.length; i++) {
-
-        allCaseToCheck.push(propsListe[i].dataset.coord)
-
-      }
-      let aroundCases = checkAroundCase(allCaseToCheck)
+        /*si le bouton hint activé,la verif de la case ne se fait pas mais on affichera la porté de tous 
+        les props du même type que la case survolée qu'on stockera dans un tableau */
+  
+        console.log(`ceci est un ${targetBehavior }`)
+        let propsListe = document.getElementsByClassName(`boardTile ${targetBehavior}`)
+        let allCaseToCheck = []
+        for(let i = 0; i < propsListe.length; i++) {
+  
+          allCaseToCheck.push(propsListe[i].dataset.coord)
+  
+        }
+        /*on récupère la liste de toutes les cases valides adjacentes aux cases insérées dans allCaseToCheck*/
+        let aroundCases = checkAroundCase(allCaseToCheck)
+        console.log(aroundCases)
+        
+  
+  
+        /*on aplique la classe correspondant à la portée du props selectioné*/      
+        aroundCases.forEach(tile => {
+          let tileToUpdate = document.getElementsByName(tile)[0]
+          if(tileToUpdate.dataset.behavior === 'basicTile') {
+            tileToUpdate.classList.add(`${targetBehavior}Range`)
+          }
+          
+        })
+  
+        
+        console.log(document.querySelectorAll(".houseRange ,.treeRange, .industryRange, .storeRange"))
 
     }
   }
+
 }
 
 const checkAroundCase = (propCasesCoord) => {
   let casesCoordListes = []
-
   /*on parcours la liste de toutes les cases contenant le même type que celui survolé grace a hint*/
   propCasesCoord.forEach(propCase => {
     let caseToCheck = parseInt(propCase)
-  /*on vérifie, pour chaque case, les 8 cases qui l'entourent aaaaaaa retravailler
-   */
-    let caseHautGauche = caseToCheck - 11
-    let caseHaut = caseToCheck - 10
-    let caseHautDroit = caseToCheck - 9
-    let caseGauche = caseToCheck - 1
-    let caseDroit = caseToCheck + 1
-    let caseBasGauche = caseToCheck + 9
-    let caseBas = caseToCheck + 10
-    let caseBasDroit = caseToCheck + 11
+    let caseHautGauche = ""
+    let caseHaut = ""
+    let caseHautDroit = ""
+    let caseGauche = ""
+    let caseDroit = ""
+    let caseBasGauche = ""
+    let caseBas = ""
+    let caseBasDroit = ""
 
-    let aroundArray = [caseHautGauche,caseHaut,caseHautDroit,caseGauche, caseDroit, caseBasGauche, caseBas, caseBasDroit]
-    let validateAroundCases = []
+    /*on vérifie, par raport aux coordonées de la case si ses cases adjacentes ne dépacent pas de la grille */
+    if(caseToCheck % 10 !== 0) {
+      caseGauche = caseToCheck - 1
+    }
+    if(propCase[1] !== "9") {
+      caseDroit = caseToCheck + 1
+    }
+    if(caseToCheck > 9) {
+      caseHaut = caseToCheck - 10
+      if(caseToCheck % 10 !== 0) {
+        caseHautGauche = caseToCheck - 11
+      }
+      if(propCase[1] !== "9") {
+        caseHautDroit = caseToCheck - 9
+      }
+    }
+    if(caseToCheck < 90) {
+      caseBas = caseToCheck + 10
+      if(caseToCheck % 10 !== 0) {
+        caseBasGauche = caseToCheck + 9
 
-    boardMap.forEach(row => row.forEach(tile=> {
+      }
+      if(propCase[1] !== "9") {
+        caseBasDroit = caseToCheck + 11
+      }
+    }
+    /*création du tableau contenant toutes les cases potentiellement autour de la case cible, transformée en
+    string pour que les coordonnées commencant par 0 (ex : 01, 02 ...) ne voient pas le 0 suprimé par la suite */
+    let aroundArray = [caseHautGauche.toString(),caseHaut.toString(),caseHautDroit.toString(),caseGauche.toString(), caseDroit.toString(), caseBasGauche.toString(), caseBas.toString(), caseBasDroit.toString()]
+    // console.log(aroundArray)
+    casesCoordListes.push(...aroundArray)
 
-    }))
-
-    console.log(`validateAroundCases pour coord ${propCase} : ${aroundArray}`)
+    /*on retire les doublons */
+    casesCoordListes = casesCoordListes.map(coord=> {
+      /*on rajoute le 0 devant les coordonées contant un chifre unique pour pour obtenir des données valdes (ex: 01,02 ...)  */
+      
+      if(coord.length === 1 ) {
+        return `0${coord}`
+      }else {
+        return coord
+      }
+    })
+    casesCoordListes = [...new Set(casesCoordListes)].sort()
+    if (casesCoordListes[0] === "") {
+      casesCoordListes.splice(0, 1);
+    }
   })
 
-  // if((propCasesCoord - 11)[0] === caseY - 1) {
-  //   console.log((propCasesCoord - 11))
-  // } else {
-  //   console.log('hors map')
-
-  // }
-  
-  
-
-  return () => casesCoordListes
+  return casesCoordListes
 }
+
+
 const cleanHover = (e) => {
   e.target.classList.remove("redTile")
 }
@@ -188,7 +235,7 @@ const update = (on) => {
                 <div className='boardRow'>
                   {
                     row.map(tile =>
-                      <div className={`boardTile ${tile.behavior}`} data-coord={tile.coord} data-behavior={tile.behavior} onClick={e => aplyProps(e)} onMouseEnter={e => checkEmpty(e)} onMouseLeave={e => cleanHover(e)}></div>
+                      <div className={`boardTile ${tile.behavior}`} name={tile.coord} data-coord={tile.coord} data-behavior={tile.behavior} onClick={e => aplyProps(e)} onMouseEnter={e => hoverCheck(e)} onMouseLeave={e => cleanHover(e)}></div>
                     )
                   }
                 </div>
